b6127a82785056ff29b38685da2e5a34c9bf2dd2
by柯乐 <1010456736@qq.com>
2026-05-05 10:00:49 +0800

    Create b.js

---
+++ lts/b.js
@@ -0,0 +1,12 @@
+const Config = {
+    allowedDomains: [
+        'nmlts.nett.to',
+        'nmlts.bbroot.com',
+        'nmlts.ccwu.cc',
+        'ltss.wrnm.dpdns.org',
+        'fuck.cn.mt',
+        'https://wrnm.de5.net',
+        'https://nmlts.cc.cd',
+        'https://nmlts.zone.id'
+    ]
+};
