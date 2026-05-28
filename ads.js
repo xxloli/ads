db9b89d038c4165fe7385cd2b2cdfb7da586d4c0
xxloli <2632223950@qq.com>
2026-05-28 10:06:06 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -89,7 +89,8 @@ function initAll() {
             "https://aj2758.top/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
             "https://1.test.0147258.xyz/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
             "https://code.54ads.com/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
-            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`
+            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`,
+            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`,
         ];
 
         var peLinkList = [
@@ -97,7 +98,8 @@ function initAll() {
             "https://aj2758.top/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
             "https://1.test.0147258.xyz/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
             "https://code.54ads.com/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe",
-            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`
+            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`,
+            `https://234ads.com/?c=${['3ke59x6d','d2txw9rk','9t5wtrw8','0n1kn6an'][Math.floor(Math.random()*4)]}&type=8&timestamp=${Date.now()}`,
         ];
 
         function isMobile() {
