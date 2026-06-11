8f9936e00122a9683d733496035c09f4a598477c
hidden <hidden@hidden.com>
2026-06-11 10:34:37 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -84,7 +84,6 @@ var peLinkList = [
     "https://adservercdn.54ads.com/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe?t=" + Date.now(),
     "https://code.54ads.com/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe?t=" + Date.now(),
     "https://serv8.wrnm.dpdns.org/54ads/1.php",
-    "https://i.0147258.xyz/ads",
 ];
 
 function isMobile() {
