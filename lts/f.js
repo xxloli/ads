65984f7f34ab314df4a1b5892a43b6c2a3423cf7
by柯乐 <1010456736@qq.com>
2026-06-16 11:10:42 +0800

    Update f.js

--- lts/f.js
+++ lts/f.js
@@ -13,6 +13,8 @@
     'https://serv8.wrnm.dpdns.org/54ads/1.php',
     'https://code.54ads.com/zFBG8Am-XNBj0-sEJn34F_suSS6agKTWfnfRL9QEDBdYRBI_qBxlYOU1UYbr-CvEf0dIABHRe',
     'https://i.0147258.xyz/ads',
+    'https://serv8.wrnm.dpdns.org/54ads/2.php',
+    'https://sqbq.chinxuan.com/upload/image/20260403/54.php',
   ];
 
   function pickRandomUrl() {
