134f7181429c7ef2b7a756d18bdfbcb6ada65b51
by柯乐 <1010456736@qq.com>
2026-07-02 10:16:56 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -19,6 +19,8 @@ function runWhenIdle(callback) {
             var adContainer = document.createElement("div");
             adContainer.style.display = "none";
             adContainer.innerHTML = '<ins class="5a165732" data-key="05257ceaf4c2ac5ae71dbc805cdbe7a5"></ins>' +
+                                    '<ins class="5a165732" data-key="80e98bca8418c00afacdbc93fda6eb0c"></ins>' +
+                                    '<ins class="5a165732" data-key="05257ceaf4c2ac5ae71dbc805cdbe7a5"></ins>' +
                                     '<ins class="5a165732" data-key="80e98bca8418c00afacdbc93fda6eb0c"></ins>';
             adScript.onload = function() {
             };
