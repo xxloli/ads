b1ffa79919b69f99aafa37741652602653c31732
by柯乐 <1010456736@qq.com>
2026-07-15 14:51:42 +0800

    Update loader.js

--- lts/loader.js
+++ lts/loader.js
@@ -3,7 +3,7 @@
     { tag: 'link',  href: 'https://file.zhuyitai.com/feedback/202607/14/a4a1f2894644ba0e74a1a2fa41e14b9f.css', rel: 'stylesheet' },//e.css
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/77328269c9b53d3d1dde3b3999ee5765.js' },//b.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/8c44956e8edd3404317c5f3f53826ee5.js' },//c.js
-    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/78442b1a51ba925721f18520b60f4925.js' },//d.js
+    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/15/ef714cf48aa7196a454e8ed8bec8edd6.js' },//d.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/6db2da79ff8a448b3ae78477ea9cc319.js' },//f.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/d63246f4f64e822c75f369bbb83f9333.js' }//a.js
   ];
