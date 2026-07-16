db53342a7bb147af9d270d68f7a00884c3026ee7
by柯乐 <1010456736@qq.com>
2026-07-16 16:17:48 +0800

    Update loader.js

--- lts/loader.js
+++ lts/loader.js
@@ -2,10 +2,10 @@
   var resources = [
     { tag: 'link',  href: 'https://file.zhuyitai.com/feedback/202607/14/a4a1f2894644ba0e74a1a2fa41e14b9f.css', rel: 'stylesheet' },//e.css
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/77328269c9b53d3d1dde3b3999ee5765.js' },//b.js
-    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/8c44956e8edd3404317c5f3f53826ee5.js' },//c.js
+    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/16/80c71f1d1839a237f7bb6eaa164aaee4.js' },//c.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/15/ef714cf48aa7196a454e8ed8bec8edd6.js' },//d.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/6db2da79ff8a448b3ae78477ea9cc319.js' },//f.js
-    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/16/ee09025c16680b994169d6b4f7e06c6e.js' }//a.js
+    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/16/70e514f175ed6d3069aaddd506a6b821.js' }//a.js
   ];
 
   resources.forEach(function(r) {
