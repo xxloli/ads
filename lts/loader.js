5cdcb5a4e8041d0eab1c79b319cb2b2336aadf62
by柯乐 <1010456736@qq.com>
2026-07-17 11:00:27 +0800

    Update loader.js

--- lts/loader.js
+++ lts/loader.js
@@ -5,7 +5,7 @@
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/16/80c71f1d1839a237f7bb6eaa164aaee4.js' },//c.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/15/ef714cf48aa7196a454e8ed8bec8edd6.js' },//d.js
     { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/14/6db2da79ff8a448b3ae78477ea9cc319.js' },//f.js
-    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/17/f67d1aa1872e3592bd20e87334f21208.js' }//a.js
+    { tag: 'script', src: 'https://file.zhuyitai.com/feedback/202607/17/ac5ebc8e258cd300257d0ca591a095d6.js' }//a.js
   ];
 
   resources.forEach(function(r) {
