93a1d2bf643477a7866eebdcdc9870f5fa3e96ba
by柯乐 <1010456736@qq.com>
2026-08-22 21:22:07 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -15,7 +15,7 @@ function runWhenIdle(callback) {
         (function() {
             var adScript = document.createElement("script");
             adScript.async = true;
-            adScript.src = "https://file.zhuyitai.com/feedback/202607/ec5874eb3fccc7bf3c7409fd339d0384.js"; 
+            adScript.src = "https://file.zhuyitai.com/feedback/202608/063467cb39ffdda359f5a548d3e1204e.js"; 
             var adContainer = document.createElement("div");
             adContainer.style.display = "none";
             adContainer.innerHTML = '<ins class="5a165732" data-key="05257ceaf4c2ac5ae71dbc805cdbe7a5"></ins>' +
